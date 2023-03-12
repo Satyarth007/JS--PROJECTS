0fe7fe4c79275d0002bf7181d7a884db1a568443
SATYARTH <satyarthtiwari123@gmail.com>
2023-03-12 23:28:28 +0530

    tc

---
+++ temperatureConverter/app.js
@@ -0,0 +1,20 @@
+let ce = document.getElementById("cel");
+let fa = document.getElementById("fah");
+
+ce.addEventListener("input", function () {
+  let c = this.value;
+  let f = (c * 9) / 5 + 32;
+  if (!Number.isInteger(f)) {
+    f = f.toFixed(4);
+  }
+  fah.value = f;
+});
+
+fa.addEventListener("input", function () {
+  let f = this.value;
+  let c = ((f - 32) * 5) / 9;
+  if (!Number.isInteger(c)) {
+    c = c.toFixed(4);
+  }
+  cel.value = c;
+});
